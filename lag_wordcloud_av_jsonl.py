d144164366bae76ae2654240a16d9a310204777c
Woldelig <christoffer.wold@gmail.com>
2018-10-27 00:16:58 +0200

    tweet_mask

--- lag_wordcloud_av_jsonl.py
+++ lag_wordcloud_av_jsonl.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from argparse import ArgumentParser
 from nltk.corpus import stopwords
 from wordcloud import WordCloud
+from skimage.io import imread
+
 
 def get_parser():
     parser = ArgumentParser()
@@ -18,17 +20,21 @@ if __name__ == '__main__':
 
     all_posts = []
     with open(fname, encoding='UTF-8') as f:
-        fil = json.loads(f.read())
+        """fil = json.loads(f.read())
 
         for objekt in fil['rader']:
-            all_posts.append(objekt.get('text', ''))
-        """for line in f:
+            all_posts.append(objekt.get('text', ''))"""
+        for line in f:
             post = json.loads(line)
-            all_posts.append(post.get('text', '')) #bytt om til attributtet du ønsker og lage wordcloud av. Twitter: text, fb:message"""
+            for hashtag in post["entities"]["hashtags"]:
+                all_posts.append(post.get(hashtag['text'].lower(), ''))
+            all_posts.append(post.get('text', '')) #bytt om til attributtet du ønsker og lage wordcloud av. Twitter: text, fb:message
     text = ' '.join(all_posts)
     stop_list = ['get', 'title', 'titles', 'www', 'https', 'http', 'com', 'youtube'] #ord som man ikke skal ha med
     stop_list.extend(stopwords.words('english'))
-    wordcloud = WordCloud(stopwords=stop_list).generate(text)
+    tweet_mask = imread("D:\\pythontest\\pythonScript\\mask\\twitter_mask.png")
+
+    wordcloud = WordCloud(stopwords=stop_list, background_color="white", mask = tweet_mask).generate(text) #kan legge til Mask for å forme wordclouden!
     plt.imshow(wordcloud)
     plt.axis("off")
-    plt.savefig('wordcloud_{}.png'.format(args.fil), dpi=1000) #dpi avgir skarpheten på bildet
+    plt.savefig('wordcloud_{}.png'.format(args.fil), dpi=300) #dpi avgir skarpheten på bildet
