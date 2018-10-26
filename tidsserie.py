f51913ce92208a1676c784120adee0e643a2929c
Woldelig <christoffer.wold@gmail.com>
2018-10-26 23:53:47 +0200

    Tidseriediagram
    Diagram har nå bedre beskrivelse på X aksen og er smoothers

--- tidsserie.py
+++ tidsserie.py
@@ -20,25 +20,25 @@ if __name__ == '__main__':
         idx = pd.DatetimeIndex(all_dates)
         my_series = pd.Series(ones, index=idx)
 
-        per_day = my_series.resample('1Min', how='sum').fillna(0)
-        print(my_series.head())
-        print(per_day.head())
+        per_day = my_series.resample('1H').sum().fillna(0)
         
         fig, ax = plt.subplots()
         ax.grid(True)
         ax.set_title("Tweet frekvens for #Worlds2018")
         
-        day = mdates.DayLocator(interval=1)
-        date_formatter = mdates.DateFormatter('%d')
+        day = mdates.DayLocator(interval=1) #Setter et tick per dag i måneden på x aksen
+        date_formatter = mdates.DateFormatter('%d-%b')
 
-        datemin = datetime(2018, 10, 5, 13, 0)
-        datemax = datetime(2018, 10, 15, 19, 30)
+        datemin = datetime(2018, 10, 5, 13, 0) #Fra dato
+        datemax = datetime(2018, 10, 15, 19, 30) #Til dato
 
         ax.xaxis.set_major_locator(day)
         ax.xaxis.set_major_formatter(date_formatter)
         ax.set_xlim(datemin, datemax)
         max_freq = per_day.max()
-        ax.set_ylim(0, max_freq+20)
+        ax.set_ylim(0, max_freq+200) # +200 så vi får litt padding til taket av grafen
         ax.plot(per_day.index, per_day)
+        plt.xticks(rotation='vertical') # setter x akse labels vertikalt så de er leselige
+        plt.tight_layout() # Gjør så label har platt i png
 
-        plt.savefig('tweet_time_series.png', dpi=1600)
+        plt.savefig('tweets_tid_serie.png', dpi=1600)
